21928e5a81f6fc4d6482737389e967f1dd4efd59
Henri Heyden <henri@heyden5.de>
2022-08-20 00:06:59 +0200

    Added some extra lines to make it more readable

--- install-jdks.sh
+++ install-jdks.sh
@@ -1,18 +1,18 @@
 version=""
 
 echo "please enter jdk version you want to install"
-echo "(like 18 for the jdk-18)"
+echo "(like 18 for the jdk-18)\n\n"
 read version
 
-echo "downloading oracle's jdk-$version"
+echo "downloading oracle's jdk-$version\n\n"
 mkdir ./temp
 cd ./temp
 wget "https://download.oracle.com/java/$version/latest/jdk-"$version"_linux-x64_bin.deb"
 
-echo "installing jdk-$version"
+echo "installing jdk-$version\n\n"
 sudo apt install "./jdk-$version\linux-x64_bin.deb"
 
-echo "adding jdk-$version to your PATH"
+echo "adding jdk-$version to your PATH\n\n"
 sudo ln -fs "/usr/lib/jvm/jdk-$version/bin/* /bin/"
 
 cd ..
